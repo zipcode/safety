a9ad3392889deedaaa825024430c6d3a89b1e697
@zip <->
2014-08-18 16:10:10 -0700

    Added login sounds; fixed autologin order

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,19 +10,25 @@ amixer sset Master 0dB unmute
 amixer sset "Master Mono" 0dB unmute
 amixer sset PCM 0dB unmute
 
-# Autologin to console
-sed -i 's/getty/getty -a vagrant/' /etc/init/tty1.conf
-initctl stop tty1
-initctl start tty1
-
 cat > /home/vagrant/.bash_profile <<EOF
 # Created by bootstrap.sh
 if [ ! -e /vagrant/safety.3gpp ]; then
   cclive -O /vagrant/safety.3gpp https://www.youtube.com/watch?v=IacSW-rmxsc
 fi
-if [ "\`tty\`" == "/dev/tty1" ] && [ ! -f /dev/shm/.noreplay ]; then
-  touch /dev/shm/.noreply
-  mplayer -fs -vo fbdev /vagrant/safety.3gpp
-fi;
+if [ ! -e /vagrant/login.3gpp ]; then
+  cclive -O /vagrant/login.3gpp https://www.youtube.com/watch?v=7nQ2oiVqKHw
+fi
+if [ "\`tty\`" == "/dev/tty1" ] && [ ! -f /vagrant/.noreplay ]; then
+  touch /vagrant/.noreplay
+  mplayer -really-quiet -fs -vo fbdev /vagrant/safety.3gpp 2>/dev/null
+else
+  mplayer -really-quiet -vo none /vagrant/login.3gpp 2>/dev/null
+fi
 EOF
 chown vagrant /home/vagrant/.bash_profile
+
+# Autologin to console
+sed -i 's/getty/getty -a vagrant/' /etc/init/tty1.conf
+initctl stop tty1
+initctl start tty1
+
